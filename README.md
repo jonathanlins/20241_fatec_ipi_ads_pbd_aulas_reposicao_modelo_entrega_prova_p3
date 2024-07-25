8075a76fb81530bad243cdcdde3d493c1fea2146
Rodrigo Bossini <professorbossini@gmail.com>
2024-07-25 14:52:11 -0300

    chore: adjusts test instructions

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-Nomes dos integrantes completos e sem abreviações e seus RAS
+Nomes dos integrantes completos, em ordem alfabética e sem abreviações e seus RAS
 Somente serão considerados os integrantes que tiverem ingressado com, no máximo, 30 minutos de atraso
--- script.sql
+++ script.sql
@@ -1,25 +1,36 @@
 -- ----------------------------------------------------------------
--- 1 Base de dados e criação de tabela
---escreva a sua solução aqui
+--NÃO APAGUE NADA DESTE ARQUIVO. FAZÊ-LO IMPLICARÁ EM ANULAÇÃO DA SUA PROVA.
+-- ----------------------------------------------------------------
+
+--1 Base de dados e criação de tabela
+--A base a ser utilizada pode ser obtida a partir do link a seguir. 
+--https://www.kaggle.com/datasets/csafrit2/higher-education-students-performance-evaluation
+--Ela deve ser importada para uma base de dados gerenciada pelo PostgreSQL. Os dados devem ser armazenados em uma tabela apropriada para as análises --desejadas. Você deve identificar as colunas necessárias, de acordo com a descrição de cada item da prova. Além, é claro, de uma chave primária (de auto --incremento). Neste item, portanto, você deve desenvolver o script de criação da tabela.
+--SUA SOLUÇÃO DO ITEM 1 ABAIXO:
 
 
 -- ----------------------------------------------------------------
--- 2 Resultado em função da formação dos pais
---escreva a sua solução aqui
+--2 Leitura frequente
+--Utilize um cursor não vinculado para exibir a faixa etária de cada aluno que tenha frequẽncia de leitura de materiais não científicos alta.
+--SUA SOLUÇÃO DO ITEM 2 ABAIXO:
 
 
 -- ----------------------------------------------------------------
--- 3 Resultado em função dos estudos
---escreva a sua solução aqui
+--3 Aprovação sem ver aula
+--Utilize um cursor com query dinâmica para mostrar todos os dados de cada estudante aprovado que nunca viu aula. No final, exiba a quantidade de estudantes.
+--SUA SOLUÇÃO DO ITEM 3 ABAIXO:
 
 
 -- ----------------------------------------------------------------
--- 4 Salário versus estudos
---escreva a sua solução aqui
+-- 4 Transporte versus resultado
+--Utilize um cursor vinculado para exibir todos os dados de cada estudante que vai de ônibus ou bicicleta para a faculdade e que tenha sido aprovado. No --final, exiba quantos tiraram nota AA.
+--SUA SOLUÇÃO DO ITEM 4 ABAIXO:
 
 
 -- ----------------------------------------------------------------
--- 5. Limpeza de valores NULL
---escreva a sua solução aqui
+--5. Limpeza de valores NULL
+--Escreva um cursor não vinculado para a remoção de todas as tuplas que possuam o valor NULL em pelo menos um de seus campos. A remoção deve ser feita de --baixo para cima e, antes dela, cada tupla deve ser exibida. A seguir, de cima para baixo, exiba cada tupla remanescente.
+--SUA SOLUÇÃO DO ITEM 5 ABAIXO:
+
 
 -- ----------------------------------------------------------------
